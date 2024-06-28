f3a46472c254a6a1c82415d78e2a24b2831fbe3d
Johann Leblanc <johannlb.pro@gmail.com>
2024-06-28 18:47:51 +0200

    Section oscar et rotation fleurs

---
+++ templates/oscars.php
@@ -0,0 +1,4 @@
+<section class="oscars">
+    <h3> <span class="titleFade">Fleurs d’oranger & chats errants<br> est nominé aux Oscars Short<br> Film Animated de 2022 !</span> </h3>
+    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/oscars-2022.png'; ?>" alt="Logo des oscars">
+</section>
